2f32a6bf61d3d86dd77e293bbbe535593a3c9ea0
Armin Reichert <armin.reichert@web.de>
2019-05-06 08:42:32 +0200

    Set default timezone

--- mod_google_calendar_feed/helper.php
+++ mod_google_calendar_feed/helper.php
@@ -8,6 +8,7 @@ class ModGoogleCalendarFeedHelper
 
     public static function getEvents($clientID, $calendarID, $maxItemCount)
     {
+        date_default_timezone_set('Europe/Berlin'); //TODO should be set in php.ini
         $client = new Google_Client();
         $client->setDeveloperKey($clientID);
         $calendar = new Google_Service_Calendar($client);
